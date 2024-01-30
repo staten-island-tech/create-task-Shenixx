f528909c0bf503417d94bf62b991bca43defd3d7
Shenixx <113193025+Shenixx@users.noreply.github.com>
2024-01-29 20:13:33 -0500

    2321321

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const DOMSelectors = {
     box: document.getElementById("container"),
+    result: document.getElementById("results"),
     submit: document.getElementById("submitBtn"),
     submitAmount: document.getElementById("submitAmount"),
     numberOfQuestion: document.getElementById("numberOfQuestion"),
     answerInputs: document.getElementsByClassName("answerInput"),
+
 };
 
 const questions = [];
 const userAnswers = [];
+const answersB = [];
 
 function getNumber(){
     let numberA = Math.floor(Math.random() * 1000);
@@ -25,36 +28,69 @@ function display(data){
     DOMSelectors.box.insertAdjacentHTML("beforeend", `What is ${data.numberA} + ${data.numberB}`)
 
     DOMSelectors.box.insertAdjacentHTML("beforeend", ` 
-    <input type="text" class="answerInput" placeholder="Answer" data-answer=${data.numberA + data.numberB}> 
+    <input type="text" class="answerInput" placeholder="Answer"> 
     <br><br>`);
+
+    let answers = data.numberA + data.numberB
+    answersB.push(answers);
 };
 
 
 DOMSelectors.submitAmount.addEventListener("click", function () {
-    numOfQ = DOMSelectors.numberOfQuestion.value
+    numOfQ = DOMSelectors.numberOfQuestion.value;
+    
+    userAnswers.length = 0
+    questions.length = 0;
     DOMSelectors.box.innerHTML = "";
+    
 
-    for (let i = 0; i < numOfQ; i++){
+    for (let i = 0; i < numOfQ; i++) {
         const num = getNumber();
         questions.push(num);
     }
 
-    questions.forEach((question)=>{
+    questions.forEach((question) => {
         display(question);
-    })
-    
+    });
+
     return questions;
 });
 
+let checkAnswers = function (arr1, arr2, arr3) {
+    let wrong = 0;
+    let right = 0;
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] === arr2[i]) {
+            right++;
+            DOMSelectors.result.insertAdjacentHTML(
+                "beforeend",
+                `You got ${arr3[i].numberA} + ${arr3[i].numberB} = ${arr3[i].numberA + arr3[i].numberB} right. You got ${arr2[i]}<br>`
+            );
+        } else {
+            wrong++;
+            DOMSelectors.result.insertAdjacentHTML(
+                "beforeend",
+                `You got ${arr3[i].numberA} + ${arr3[i].numberB} = ${arr3[i].numberA + arr3[i].numberB} wrong. You got ${arr2[i]}<br>`
+            );
+        }
+    }
+
+    DOMSelectors.result.insertAdjacentHTML("beforeend", `you got ${right} right and ${wrong} wrong`);
+};
 
 DOMSelectors.submit.addEventListener("click", function(){  
     for (let i = 0; i < DOMSelectors.answerInputs.length; i++) {
         let userAnswer = DOMSelectors.answerInputs[i].value;
         userAnswers.push(userAnswer);
+
     }
 
-    console.log(questions);
-    console.log(userAnswers);
+    // console.log(answersB);
+    // console.log(userAnswers);
+
+
+    checkAnswers(answersB,userAnswers,questions);
 })
 
 
